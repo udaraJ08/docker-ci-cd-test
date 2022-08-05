07bb0c744caef20a1982fe0342c2532858842e2a
Udara J(SiniSTeR) <67888873+udaraJ08@users.noreply.github.com>
2022-08-05 16:46:24 +0530

    first commit

--- src/pages/ButtonComp/ButtonComp.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-const ButtonComp = () => {
-
-    return <h1>This is the button comp</h1>
-}
-
-export default ButtonComp
--- src/pages/ButtonComp/__test__/ButtonComp.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ButtonComp from "../ButtonComp";
-import {render, screen} from "@testing-library/react";
-
-it('Rendered without crashing',() => {
-    render(<ButtonComp />)
-    const text = screen.getByText('This is the button comps')
-    expect(text).toBeInTheDocument()
-});
